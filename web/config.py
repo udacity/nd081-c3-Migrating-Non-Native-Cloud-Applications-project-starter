84ecf81bbc1c07ee14175a41c2fad27b82099fde
amitjolly82 <75559575+amitjolly82@users.noreply.github.com>
2021-07-28 22:10:21 -0700

    Update config.py
    commit

--- web/config.py
+++ web/config.py
@@ -4,10 +4,10 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL="migrationudacity.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="mypgadmin@migrationudacity" #TODO: Update value
+    POSTGRES_PW="Jethalal123@"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
